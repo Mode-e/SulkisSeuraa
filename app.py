0edaddd2908a4699bb572f9aeadcc8445f609f04
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-18 10:43:53 +0300

    Add register funtion

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+        return redirect("/login")
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
