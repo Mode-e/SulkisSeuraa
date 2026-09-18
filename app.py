fcf9cf5a940ebf72d624316bbcca483be6d50565
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-18 10:49:30 +0300

    Add login funktion

--- app.py
+++ app.py
@@ -11,6 +11,24 @@ app.secret_key = "super_salainen_avain_tähän"
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+        
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    sql = "SELECT password_hash FROM users WHERE username = ?"
+    password_hash = db.query(sql, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/")
+    else:
+        return "VIRHE: väärä tunnus tai salasana"
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
