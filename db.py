1cd66d55c5929e2e9c491eaad1630afb63c06ee5
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-21 23:43:04 +0300

    Fix pylint warnings

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ def get_connection():
     con.row_factory = sqlite3.Row
     return con
 
-def execute(sql, params=[]):
+def execute(sql, params=None):
     con = get_connection()
     try:
         result = con.execute(sql, params)
@@ -22,7 +22,7 @@ def execute(sql, params=[]):
 def last_insert_id():
     return g.last_insert_id
 
-def query(sql, params=[]):
+def query(sql, params=None):
     con = get_connection()
     result = con.execute(sql, params).fetchall()
     con.close()
