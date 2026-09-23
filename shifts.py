7d396839b759f2f82714db758def46c2583f5bc7
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-23 14:44:50 +0300

    Add query for signer_up_players

--- shifts.py
+++ shifts.py
@@ -44,9 +44,10 @@ def get_past_shifts_by_user(user_id, time_now):
 
 def get_shift(shift_id):
     sql = """
-        SELECT id, user_id, location, time, player_level, player_count, total_players 
-        FROM shifts 
-        WHERE id = ?
+        SELECT S.id, S.user_id, S.location, S.time, S.player_level, S.player_count, S.total_players, U.username
+        FROM shifts S
+        JOIN users U ON S.user_id = U.id
+        WHERE S.id = ?
     """
     result = db.query(sql, [shift_id])
 
@@ -87,3 +88,12 @@ def find_shifts(time_now, location=None, day=None, player_level=None, total_play
     sql += " ORDER BY shifts.time ASC"
 
     return db.query(sql, params)
+
+def get_signed_up_players(shift_id):
+    sql = """
+        SELECT users.username 
+        FROM signups
+        JOIN users ON signups.user_id = users.id
+        WHERE signups.shift_id = ?
+    """
+    return db.query(sql, [shift_id])
