d195e2bbcb3c1f3c873b334a68f26b3373439e68
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-23 14:43:56 +0300

    Add signups table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS shifts (
     total_players INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE IF NOT EXISTS signups (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    shift_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (shift_id) REFERENCES shifts(id) ON DELETE CASCADE,
+    UNIQUE(user_id, shift_id)
+);
+
