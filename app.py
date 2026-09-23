3fb502b0c25925dff0e40a07d7f78885ac68aa1e
Teemu Möttönen <teemu.mode@yahoo.fi>
2026-09-23 12:04:52 +0300

    Update get_form_data to handle search

--- app.py
+++ app.py
@@ -11,12 +11,31 @@ app.secret_key = secret_key
 
 
 def get_form_data(word = None):
+    if word == "search":
+        return {
+            "location": request.args.get("location", "").strip(),
+            "day": request.args.get("day", "").strip(),
+            "player_level": request.args.get("player_level", "").strip(),
+            "total_players": request.args.get("total_players", "").strip()
+        }
+
     shift = {
         "location": request.form["location"],
         "time": f"{request.form["day"]} {request.form["hours"]}:{request.form["minutes"]}",
         "player_level": request.form["player_level"],
         "total_players": request.form["total_players"]
     }
+
+    if word in ["user_login", "user_create"]:
+        user_data = {
+            "username": request.form["username"],
+            "password1": request.form["password1"]
+        }
+
+        if word == "user_create":
+            user_data["password2"] = request.form["password2"]
+
+        return user_data
 
     if word == "create":
         shift["user_id"] = session["user_id"]
